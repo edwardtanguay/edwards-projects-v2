b762ae023a5faedb5be0b555f082925ab1316e13
Edward Tanguay (windows) <edward@tanguay.info>
2025-12-22 00:14:43 +0100

    ...invite text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+import Link from "next/link";
 
 export default function Home() {
 	return (
@@ -11,6 +12,30 @@ export default function Home() {
 					<p className="text-lg text-gray-400 leading-relaxed">
 						This site shows the status of all my software
 						development projects.
+					</p>
+
+					<p className="text-gray-300 mt-4">
+						Check out my{" "}
+						<Link href="/active-projects" className="text-blue-400 hover:text-blue-300 underline">
+							active projects
+						</Link>
+						, see what{" "}
+						<Link href="/current-tasks" className="text-blue-400 hover:text-blue-300 underline">
+							current tasks
+						</Link>{" "}
+						I'm working on, view{" "}
+						<Link href="/upcoming-tasks" className="text-blue-400 hover:text-blue-300 underline">
+							upcoming tasks
+						</Link>
+						, browse{" "}
+						<Link href="/finished-tasks" className="text-blue-400 hover:text-blue-300 underline">
+							finished tasks
+						</Link>
+						, or read more{" "}
+						<Link href="/about" className="text-blue-400 hover:text-blue-300 underline">
+							about this site
+						</Link>
+						.
 					</p>
 				</div>
 			</div>
