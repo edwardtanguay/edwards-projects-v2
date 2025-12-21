2469c4316d6206937b14b4d87aec9b97c96fb9b7
Edward Tanguay (windows) <edward@tanguay.info>
2025-12-21 23:57:44 +0100

    ....basic finished

--- src/app/finished-tasks/page.tsx
+++ src/app/finished-tasks/page.tsx
@@ -1,3 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
+import tasks from "../../../parseddata/tasks.json";
+import Task from "../../components/Task";
+
+const finishedTasks = tasks.filter(task => task.stage === "finished").sort((a, b) => (a.endDateTime < b.endDateTime ? 1 : -1));
+
+
 export default function Company() {
 	return (
 		<div className="p-8 md:p-12 max-w-4xl">
@@ -9,6 +16,12 @@ export default function Company() {
 					<p className="text-lg text-gray-400 leading-relaxed">
 					These are tasks that I have completed in various projects.
 					</p>
+				</div>
+
+				<div className="space-y-4">
+					{finishedTasks.map((task) => (
+						<Task key={task.suuid} task={task} />
+					))}
 				</div>
 
 			</div>
